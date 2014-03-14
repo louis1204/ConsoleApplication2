06b35806845cc9adf6f18d1dd6d472c1ce8c77f2
Louis <louis1204@gmail.com>
2014-03-13 21:02:45 -0700

    Init

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool control = true;
+            while (control)
+            {
+                Console.WriteLine("Enter decimal number: ");
+                string num = Console.In.ReadLine();
+                Console.Out.WriteLine(Convert.ToString(int.Parse(num), 2));
+            }
+        }
+    }
+}
